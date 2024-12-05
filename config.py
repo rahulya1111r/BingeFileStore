677b3ff16aa6881e81cf6635b1011f87d7eb5dd3
Matt Murdock <123798745+xr-coder@users.noreply.github.com>
2024-12-05 15:56:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ DISABLE_CHANNEL_BUTTON = True
 
 SHORTENER_API = os.environ.get("SHORTENER_API" , "")
 
-SHORTENER_SITE = "publicearn.in"
+SHORTENER_SITE = "gyanilinks.com"
 
 HOW_TO_DOWNLOAD = "https://telegram.me/How_To_Download_From_Linkz/4"
 
